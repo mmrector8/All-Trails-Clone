80dda6a98835b1e7b33098e307a287dc98dead1d
Morgan Rector <mmrector@yahoo.com>
2022-12-27 14:34:36 -0800

    Login fixed, error messages updating

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -16,7 +16,7 @@ class Api::SessionsController < ApplicationController
       login!(@user)
       render 'api/users/show'
     else
-      render json: { errors: ['The provided credentials were invalid.'], status: :unauthorized}
+      render json: { errors: ['The provided credentials were invalid, please try again.'] }, status: :unauthorized
     end
   end
 
