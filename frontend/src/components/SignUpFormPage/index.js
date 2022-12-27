b0897ab23aa6dcdddea0e0c6cd8c717fba8a5778
Morgan Rector <mmrector@yahoo.com>
2022-12-27 11:31:35 -0800

    Sign up modal styling added

--- frontend/src/components/SignUpFormPage/index.js
+++ frontend/src/components/SignUpFormPage/index.js
@@ -1,7 +1,8 @@
 import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 import React, { useState } from 'react'
 import * as sessionActions from '../../store/session'
-import { Redirect } from 'react-router-dom'
+import { Redirect, useHistory } from 'react-router-dom'
 import './SignUpForm.css'
 
 const SignUpForm = () => {
@@ -11,6 +12,7 @@ const SignUpForm = () => {
     const [errors, setErrors] = useState([])
     const [confirmPassword, setConfirmPassword] = useState("")
     const dispatch = useDispatch()
+    const history = useHistory()
     const sessionUser = useSelector(state => state.session.user)
     
      if (sessionUser) return <Redirect to="/" />
@@ -37,27 +39,38 @@ const SignUpForm = () => {
         }
         return setErrors(["passwords must match"])
     }
+
     return (
         <>
-            <h1 className='formTitle'>Sign Up Form</h1>
-            <form onSubmit={handleSubmit}>
-                <div className="formElements">
-                <label>Username: 
-                    <input type="text" value={username} onChange={((e)=> setUsername(e.target.value))}></input>
-                </label>
-                <label>Email:
-                    <input type="text" value={email} onChange={((e) => setEmail(e.target.value))}></input>
-                </label>
-                <label>Password:
-                    <input type="password" value={password} onChange={((e) => setPassword(e.target.value))}></input>
-                </label>
-                <label>Confirm Password:
-                        <input type="password" value={confirmPassword} onChange={((e) => setConfirmPassword(e.target.value))}></input>
-                </label>
-                <p>{errors}</p>
+            <div className='sign-up-form'>
+            <div className='sign-up-header'>
+                <i class="fa-solid fa-mountain-city" id='logo'></i>
+            </div>
+            <div className='sign-up-title'>
+                <h1 className='formTitle'>Create your free account</h1>
+            </div>
+            <div className='sign-up-body'>
+                <form onSubmit={handleSubmit}>
+                    <div className="formElements">
+                    <label>Username: 
+                        <input type="text" value={username} onChange={((e)=> setUsername(e.target.value))}></input>
+                    </label>
+                    <label>Email:
+                        <input type="text" value={email} onChange={((e) => setEmail(e.target.value))}></input>
+                    </label>
+                    <label>Password:
+                        <input type="password" value={password} onChange={((e) => setPassword(e.target.value))}></input>
+                    </label>
+                    <label>Confirm Password:
+                            <input type="password" value={confirmPassword} onChange={((e) => setConfirmPassword(e.target.value))}></input>
+                    </label>
+                    <p>{errors}</p>
+                    </div>
+                    <button>Sign Up!</button>
+                </form>
+                 <p id='link-to-login-form'>Already have an account? <Link to="/login">Login</Link></p>
                 </div>
-                <button>Sign Up!</button>
-            </form>
+            </div>
         </>
     )
 }
