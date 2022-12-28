e2e66055026b06ef03e3b4cb4ab692d9f49ae603
Morgan Rector <mmrector@yahoo.com>
2022-12-28 15:28:47 -0800

    Backgrounds updated and scaling properly

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -4,7 +4,7 @@ import * as sessionActions from '../../store/session'
 import { Redirect, useHistory } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import './LoginForm.css'
-// import goldengate from "../../images/goldengate.jpg"
+import goldengate from "../../images/sign-in-background.jpg"
 import aerialgg  from "../../images/aerialgg.jpg"
 import ggbridge2 from "../../images/ggbridge2.jpg"
 // import pacifica from "../../images/pacifica.jpg"
@@ -47,35 +47,36 @@ const LoginFormPage = ()=>{
 
     return (
         <> 
-            <div className='login-form'>
-                <div className='login-header'>
-                    <i className="fa-solid fa-mountain-city" id='logo'></i>
-                </div>
-                <div className='login-title'>
-                    <h1 className='formTitle'>Welcome back. <br></br>Log in and start exploring.</h1>
-                </div>
-                <div className='login-body'>
-                        <form onSubmit={handleSubmit}>
-                            <div className='form-elements'>
-                            <ul>
-                                {errors.map((error) => (
-                                    <li key={error}>{error}</li>
-                                ))}
-                            </ul>
-                                <input type ='text' value={credential} onChange={((e)=> setCredential(e.target.value))} placeholder="Username or email" className='input-value'></input>
-                            <input type='password' value={password} onChange={((e) => setPassword(e.target.value))} placeholder="Password" className='input-value'></input>
-                            <button type='submit' className='login-form-buttons'>Log in</button>
-                            </div>
-                        </form>
-                            <div className ='form-elements'>
-                                 <button onClick={demoUser} className ='login-form-buttons'>Demo Login</button>
-                                <p id='link-to-sign-up-form'>Don't have an account? <Link to="/login" className='signup-link'>Sign up for free</Link></p>
-                            </div>
-                </div>
-            </div>
-            <div id='background-image-container-login'>
-                <img src={aerialgg} className='background-image-login'></img>
-                <img src={ggbridge2} className='background-image-login'></img>
+            <div className='background-image-container-login'>
+                <div className='login-form'>
+                    <div className='login-header'>
+                        <i className="fa-solid fa-mountain-city" id='logo'></i>
+                    </div>
+                    <div className='login-title'>
+                        <h1 className='formTitle'>Welcome back. <br></br>Log in and start exploring.</h1>
+                    </div>
+                    <div className='login-body'>
+                            <form onSubmit={handleSubmit}>
+                                <div className='form-elements'>
+                                <ul>
+                                    {errors.map((error) => (
+                                        <li key={error}>{error}</li>
+                                    ))}
+                                </ul>
+                                    <input type ='text' value={credential} onChange={((e)=> setCredential(e.target.value))} placeholder="Username or email" className='input-value'></input>
+                                <input type='password' value={password} onChange={((e) => setPassword(e.target.value))} placeholder="Password" className='input-value'></input>
+                                <button type='submit' className='login-form-buttons'>Log in</button>
+                                </div>
+                            </form>
+                                <div className ='form-elements'>
+                                    <button onClick={demoUser} className ='login-form-buttons'>Demo Login</button>
+                                    <p id='link-to-sign-up-form'>Don't have an account? <Link to="/login" className='signup-link'>Sign up for free</Link></p>
+                                </div>
+                        </div>
+                 </div>
+            
+                {/* <img src={aerialgg} className='background-image-login'></img>
+                <img src={ggbridge2} className='background-image-login'></img> */}
             </div>
         </>
     )
