4f68bb0a64efd5c0c9eda4998672167ff1388d49
Morgan Rector <mmrector@yahoo.com>
2022-12-30 11:53:14 -0800

    Hike index page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,23 @@
 import LoginFormPage from "./components/LoginFormPage";
 import SignUpForm from "./components/SignUpFormPage";
 import Navigation from "./components/Navigation";
+import HikeIndexPage from "./components/HikeIndexPage"
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <>
-    <div className="app">
         <Navigation />
+        <Route exact path="/">
+          <HikeIndexPage />
+        </Route>
         < Route path='/signup'>
           <SignUpForm />
         </Route>
         < Route path='/login'>
           <LoginFormPage />
         </Route>
-      </div>
     </>
   );
 }
---
+++ frontend/src/components/HikeIndexPage/index.js
@@ -0,0 +1,8 @@
+
+const BenchIndexPage = ()=>{
+
+    return (
+        <h1>This is the bench index page</h1>
+    )
+}
+export default BenchIndexPage
