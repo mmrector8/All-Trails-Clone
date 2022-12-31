b3753258729f9775b7c4c726b185ffcba72d3914
Morgan Rector <mmrector@yahoo.com>
2022-12-31 10:48:22 -0800

    Show Page divs added

--- frontend/src/components/HikeShowPage/index.js
+++ frontend/src/components/HikeShowPage/index.js
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { getHike, fetchHike } from "../../store/hikes";
+import placeholdermap from "../../images/placeholdermap.png"
 
 const HikeShowPage = ()=>{
     const dispatch = useDispatch();
@@ -28,6 +29,14 @@ const HikeShowPage = ()=>{
                 <p>Route Type: {hike.routeType}</p>
                 <p>{hike.description}</p>
             </div>
+            <div className='sidebar'>
+                <div className='sidebar-map'>
+                    <img src={placeholdermap}></img>
+                </div>
+                <div className="other-hikes">
+                    
+                </div>
+            </div>
             <div className="tags">
                 <ul>
                     <li> Tag 1</li>
@@ -38,6 +47,13 @@ const HikeShowPage = ()=>{
                 <ul>
                     <li>Conditions from reviews will go here</li>
                 </ul>
+            </div>
+            <div className='weather'>
+                <p>Weather modal here</p>
+            </div>
+            <div classNam="reviews">
+                <li>Review 1</li>
+                <li>Review 2</li>
             </div>
         </>
     )
