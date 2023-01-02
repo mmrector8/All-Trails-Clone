cb4c01622b013dc17c2eb8d6befc87871784b291
Morgan Rector <mmrector@yahoo.com>
2023-01-02 11:16:17 -0800

    dropdown button actually fixed:

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -27,14 +27,30 @@ const ProfileButton = ({user}) => {
         dispatch(sessionActions.logout())
     }
 
+    //    return (
+    //     <>
+    //         <div className='dropdown-menu' onMouseOver={openMenu} onMouseLeave={(e) => setShowMenu(false)}>
+    //             <button className='dropdown-button'>
+    //                 <i className="fa-solid fa-tree dropdown-icon" onMouseOver={((e) => setShowMenu(true))}></i>
+    //             </button>
+    //             {showMenu && (
+    //                    <ul className='dropdown dropdown-menu' onMouseOver={openMenu}>
+    //                     <li className='nav-item'>{user.fname}'s Profile</li>
+    //                     <li className='nav-item'>Reviews</li>
+    //                     <li><button onClick={logout} className=' nav-item logout-button'>Logout</button></li>
+    //                 </ul>
+    //             )}
+    //         </div>
+    //     </>
+    // )
     return (
         <>
-            <div class='dropdown-menu' onMouseOver={openMenu} onMouseLeave={(e) => setShowMenu(false)}>
-                <button className='dropdown-button'>
-                    <i className="fa-solid fa-tree dropdown-icon" onMouseOver={((e) => setShowMenu(true))}></i>
+            <div className='dropdown-menu' onMouseOver={((e) => setShowMenu(true))} onMouseLeave={(e) => setShowMenu(false)}>
+                <button className='dropdown-button' onMouseOver={openMenu}>
+                    <i className="fa-solid fa-tree dropdown-icon" ></i>
                 </button>
                 {showMenu && (
-                    <ul className='dropdown' onMouseOver={openMenu}>
+                    <ul className='dropdown' onMouseOver={openMenu} onMouseLeave={(e) => setShowMenu(false)}>
                         <li className='nav-item'>{user.fname}'s Profile</li>
                         <li className='nav-item'>Reviews</li>
                         <li><button onClick={logout} className=' nav-item logout-button'>Logout</button></li>
