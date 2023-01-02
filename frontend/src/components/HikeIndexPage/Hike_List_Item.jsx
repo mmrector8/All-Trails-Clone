68e8dd0815af70535c65b415b0cddbd06e30fc4c
Morgan Rector <mmrector@yahoo.com>
2023-01-01 18:15:32 -0800

    Index page is link to show page

--- frontend/src/components/HikeIndexPage/Hike_List_Item.jsx
+++ frontend/src/components/HikeIndexPage/Hike_List_Item.jsx
@@ -1,23 +1,30 @@
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 
 const HikeListItem = ({hike}) =>{
+
+    const handleClick = (e)=>{
+    //    return <Redirect to={`/hikes/${hike.id}`} />
+        return <Link to={`/hikes/${hike.id}`}> Redirect</Link>
+    }
     return (
         <>
-        <div className="hike-list-item-container">
-        <div className="hike-list-items-photos">
-            <img src="" alt="photo here"></img>
-        </div>
-        <div className="hike-list-items">
-            <p className="hike-list-item hike-difficulty">{hike.difficulty}</p>
-            <p className="hike-list-item hike-name">{hike.name}</p>
-            <p className ="hike-list-item park-name-trails">{hike.parkId}</p>
-            <p className="hike-list-item hike-city">{hike.city}</p>
-            <p className="hike-list-item hike-elevation">{hike.elevationGain} ft. elevation gain</p>
-            <p className="hike-list-item hike-duration" >{hike.duration}</p>
-            <p className="hike-list-item hike-description">{hike.description}</p>
-            <p className="hike-list-item hike-estimatedTime">{hike.estimatedTime}</p>
-        </div>
-        </div>
+        <Link to={`/hikes/${hike.id}`} id="link">
+            <div className="hike-list-item-container">
+            <div className="hike-list-items-photos">
+                <img src="" alt="photo here"></img>
+            </div>
+            <div className="hike-list-items">
+                <p className="hike-list-item hike-difficulty">{hike.difficulty}</p>
+                <div className="top-list-items">
+                    <p className="hike-list-item hike-name">{hike.name}</p>
+                    <p className ="hike-list-item park-name-trails">Park Name:{hike.parkId}</p>
+                    <p className="hike-list-item hike-duration-and-estimated-time">Length: {hike.duration} {"•"} {hike.estimatedTime}  </p>
+                </div>
+                <p className="hike-list-item hike-description">{hike.description}</p>
+                
+            </div>
+            </div>
+        </Link>
         </>
     )
 }
