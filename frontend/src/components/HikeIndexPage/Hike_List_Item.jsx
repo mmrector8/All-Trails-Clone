93db5956cefcf7fce506dd9964bc41fd493aedf5
Morgan Rector <mmrector@yahoo.com>
2023-01-03 11:36:45 -0800

    Nested link fixed in Hike Index Page

--- frontend/src/components/HikeIndexPage/Hike_List_Item.jsx
+++ frontend/src/components/HikeIndexPage/Hike_List_Item.jsx
@@ -3,9 +3,11 @@ import { Link, Redirect } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getPark, fetchPark } from "../../store/parks";
 import { useHistory } from 'react-router-dom';
+// import {useNavigate} from 'react-router-dom';
 
 const HikeListItem = ({hike }) =>{
     const dispatch = useDispatch();
+    // const navigate = useNavigate();
     const history = useHistory();
     const park = useSelector(getPark(hike.parkId))
 
@@ -17,10 +19,20 @@ const HikeListItem = ({hike }) =>{
             return null;
     }
    
+    const routeChange = ()=>{
+        let path = `/hikes/${ hike.id }`
+        history.push(path)
+        window.scrollTo({top: 0, left: 0})
+    }
+
+    const handleParkShowClick = (e)=>{
+        e.stopPropagation()
+        window.scrollTo({ top: 0, left: 0 })
+    }
 
     return (
         <>
-            <Link to={`/hikes/${hike.id}`} onClick={()=>window.scrollTo({ top: 0, left: 0})} className="link">
+            <div onClick={routeChange} className='hike-show-button'>
             <div className="hike-list-item-container">
             <div className="hike-list-items-photos">
                 <img src="" alt="photo here"></img>
@@ -29,19 +41,17 @@ const HikeListItem = ({hike }) =>{
                 <p className="hike-list-item hike-difficulty-index">{hike.difficulty}</p>
                 <div className="top-list-items">
                     <p className="hike-list-item hike-name">{hike.name}</p>
-                    {/* <button onClick={(() => history.push(`/parks/${hike.parkId}`))}>{park.name}</button> */}
-                    <Link to={`/parks/${hike.parkId}`} className ="hike-list-item park-name-trails">{park.name}</Link>
+                    <Link to={`/parks/${hike.parkId}`} onClick={handleParkShowClick} className="hike-list-item park-name-trails">{park.name}</Link>
                     <p className="hike-list-item hike-duration-and-estimated-time">Length: {hike.duration} {"•"} Est. {hike.estimatedTime}  </p>
                 </div>
                 <div className="description-container">
                     <p className="hike-list-item hike-description">
                         {hike.description.slice(0, 150)}... <button className="read-more link">Show more </button>
-                        {/* {hike.description.slice(0, 150)}... <Link to={`/hikes/${hike.id}`} className="read-more link">Show more </Link> */}
                     </p>
                 </div>
             </div>
             </div>
-        </Link>
+        </div>
         </>
     )
 }
