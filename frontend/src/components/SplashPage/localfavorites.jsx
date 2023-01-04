99fef5ab2395f271c63c55cf2c7a9fb62bb3e67d
Morgan Rector <mmrector@yahoo.com>
2023-01-04 14:43:45 -0800

    Page scrolling but fixed splash page

--- frontend/src/components/SplashPage/localfavorites.jsx
+++ frontend/src/components/SplashPage/localfavorites.jsx
@@ -5,7 +5,7 @@ const LocalFavorites = () =>{
 
     return (
         <div className='local-favorites'>
-            <h1 className="local-favorites-links">Local favorites in the <Link to={'/hikes'} onClick={window.scrollTo({ top: 0, left: 0 })} className="local-favorites-links local-fave-link">Bay Area</Link></h1>
+            <h1 className="local-favorites-links">Local favorites in the <Link to={'/hikes'} onClick={()=>window.scrollTo({ top: 0, left: 0 })} className="local-favorites-links local-fave-link">Bay Area</Link></h1>
             {/* <Carousel itemsToShow={1}>
                 <p className='local-bay-hike-images'>Img here</p>
                 <p className='local-bay-hike-images'>Img here</p>
