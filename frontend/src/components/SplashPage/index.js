1ef0a8147c3fb3dcaa7e84e46c34a993864ce3e7
Morgan Rector <mmrector@yahoo.com>
2023-01-03 17:49:49 -0800

    reasons to sign up added to splash page

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -53,20 +53,21 @@ const SplashPage = ()=>{
             
                 <div className='reasons-to-sign-up'>
                     <div className='curated-trails'>
-                        <h1>350k+</h1>
-                        <h3>curated trails</h3>
-                        <p>Discover unexpected gems, even in your own backyard.</p>
+                        <h1 className='large-reasons'>350k+</h1>
+                        <h3 className='medium-reasons'>curated trails</h3>
+                        <p className='small-reasons'>Discover unexpected gems, even in your own backyard.</p>
                     </div>
                     <div className='fellow-explorers'>
-                        <h1>40 mil</h1>
-                        <h3>fellow explorers</h3>
-                        <p>Share your adventures and learn from our global community.</p>
+                        <h1 className='large-reasons'>40 mil</h1>
+                        <h3 className='medium-reasons'>fellow explorers</h3>
+                        <p className="small-reasons">Share your adventures and learn from our global community.</p>
                     </div>
                     <div className='logged-miles'>
-                        <h1>600+ mil</h1>
-                        <h3>logged miles</h3>
-                        <p>Navigate your way and keep a record of all your travels.</p>
+                        <h1 className='large-reasons'>600+ mil</h1>
+                        <h3 className='medium-reasons'>logged miles</h3>
+                        <p className="small-reasons">Navigate your way and keep a record of all your travels.</p>
                     </div>
+                </div>
                     <div className='adventure-anywhere'>
                         <h1>Adventure anywhere</h1>
                         <p>Parks worth a look</p>
@@ -81,7 +82,7 @@ const SplashPage = ()=>{
                             <p className='park-image'>parkImg</p>
                         </div>
                     </div>
-                </div>
+               
         </>
     )
 }
