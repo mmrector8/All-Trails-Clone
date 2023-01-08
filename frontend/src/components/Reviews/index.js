871b3ae9ddffe97d9bdd02ee80b368ddefb29dbc
Morgan Rector <mmrector@yahoo.com>
2023-01-07 16:49:01 -0800

    Removed be the first to write review p

--- frontend/src/components/Reviews/index.js
+++ frontend/src/components/Reviews/index.js
@@ -1,12 +1,13 @@
 import * as reviewscss from "./reviews.css"
 import ReviewIndexItem from "./ReviewIndexItem";
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 import {useDispatch, useSelector} from 'react-redux' 
 import {getReviews } from "../../store/reviews"
 import { useParams } from "react-router-dom";
 import ReviewModalContainer from "./reviewmodalcontainer";
 
 const ReviewIndex = ({hike}) =>{
+    const [reviewsPresent, setReviewsPresent] = useState(false)
     const reviews = useSelector(getReviews)
     const currentUser = useSelector(state=> state.session.user)
 
@@ -15,6 +16,9 @@ const ReviewIndex = ({hike}) =>{
     }
 
     const averageStars = () =>{
+        if (!reviews.length){
+           return 0
+        }
         let count = 0.0;
         for(let i=0; i < reviews.length; i++){
             count += reviews[i].stars
@@ -27,17 +31,19 @@ const ReviewIndex = ({hike}) =>{
             <h1 className='reviews-title'>Reviews ({reviews.length})</h1>
                 <div className= "review-top-bucket">
                     <div className='review-avg-rating-bar'>
-                        <p>5 <i className="fa-sharp fa-solid fa-star"></i>------</p>
-                        <p>4 <i className="fa-sharp fa-solid fa-star"></i>------</p>
-                        <p>3 <i className="fa-sharp fa-solid fa-star"></i>------</p>
-                        <p>2 <i className="fa-sharp fa-solid fa-star"></i>------</p>
-                        <p>1<i className="fa-sharp fa-solid fa-star"></i>------</p>
+                        <p>5 <i className="fa-sharp fa-solid fa-star star-rating-icon"></i>------</p>
+                        <p>4 <i className="fa-sharp fa-solid fa-star star-rating-icon"></i>------</p>
+                        <p>3 <i className="fa-sharp fa-solid fa-star star-rating-icon"></i>------</p>
+                        <p>2 <i className="fa-sharp fa-solid fa-star star-rating-icon"></i>------</p>
+                        <p>1<i className="fa-sharp fa-solid fa-star star-rating-icon"></i>------</p>
                     </div>
                     <div className="avg-reviews">
-                        <h1 className='avg-stars'>{averageStars()}</h1>
-                        <p><i className="fa-sharp fa-solid fa-star"></i><i className="fa-sharp fa-solid fa-star"></i><i className="fa-sharp fa-solid fa-star"></i><i className="fa-sharp fa-solid fa-star"></i><i className="fa-sharp fa-solid fa-star"></i></p>
+                         <h1 className='avg-stars'>{averageStars()}</h1>
+                        <p><i className="fa-sharp fa-solid fa-star star-rating-icon"></i><i className="fa-sharp fa-solid fa-star star-rating-icon"></i><i className="fa-sharp fa-solid fa-star star-rating-icon"></i><i className="fa-sharp fa-solid fa-starstar-rating-icon"></i><i className="fa-sharp fa-solid fa-star star-rating-icon"></i><i className="fa-sharp fa-solid fa-star star-rating-icon"></i><i className="fa-sharp fa-solid fa-starstar-rating-icon"></i></p>
                         <p>{reviews.length} reviews</p>
+                        
                     </div>
+                     
                     <ReviewModalContainer hike={hike}/>
                 </div>
             {reviews?.map((review, i)=><ReviewIndexItem review={review} key={i}/>)}
