81e5e6fe604137cac4d0992b4027b3041c7ca4db
Morgan Rector <mmrector@yahoo.com>
2023-01-07 16:00:43 -0800

    fixed modal resizing

--- frontend/src/components/Reviews/reviewmodal.jsx
+++ frontend/src/components/Reviews/reviewmodal.jsx
@@ -40,24 +40,24 @@ const ReviewModal = ({open, onClose, hike}) =>{
                     <div className="review-form">
                     <div className="star-rating">
                         <input id="5" type="radio" name="rating" value={5} onChange={(e => setStars(e.target.value))} />
-                        <label for="5" title="5 stars" className="star-label">
-                            <i class="active fa fa-star" aria-hidden="true"></i>
+                        <label htmlFor="5" title="5 stars" className="star-label">
+                            <i className="active fa fa-star" aria-hidden="true"></i>
                         </label>
                         <input id="4" type="radio" name="rating" value={4} onChange={(e => setStars(e.target.value))} />
-                        <label for="4" title="4 stars" className="star-label">
-                            <i class="active fa fa-star" aria-hidden="true"></i>
+                        <label htmlFor="4" title="4 stars" className="star-label">
+                            <i className="active fa fa-star" aria-hidden="true"></i>
                         </label>
                         <input id="3" type="radio" name="rating" value={3} onChange={(e => setStars(e.target.value))} />
-                        <label for="3" title="3 stars" className="star-label">
-                            <i class="active fa fa-star" aria-hidden="true"></i>
+                        <label htmlFor="3" title="3 stars" className="star-label">
+                            <i className="active fa fa-star" aria-hidden="true"></i>
                         </label>
                         <input id="2" type="radio" name="rating" value={2} onChange={(e => setStars(e.target.value))} />
-                        <label for="2" title="2 stars" className="star-label">
-                            <i class="active fa fa-star" aria-hidden="true"></i>
+                        <label htmlFor="2" title="2 stars" className="star-label">
+                            <i className="active fa fa-star" aria-hidden="true"></i>
                         </label>
                         <input id="1" type="radio" name="rating" value={1} onChange={(e => setStars(e.target.value))} />
-                        <label for="1" title="1 star" className="star-label">
-                            <i class="active fa fa-star" aria-hidden="true"></i>
+                        <label htmlFor="1" title="1 star" className="star-label">
+                            <i className="active fa fa-star" aria-hidden="true"></i>
                         </label>
                     </div>
                     <div className="review-content">
@@ -74,10 +74,14 @@ const ReviewModal = ({open, onClose, hike}) =>{
                             
                     </div>
                     <label>Trail Conditions 
-                        <select value={conditions} onChange={(e => setConditions(e.target.value))}>
+                        {/* <select value={conditions} onChange={(e => setConditions(e.target.value))}>
                             <option value="Great!">Great!</option>
                             <option value="No shade">No shade</option>
-                        </select>
+                        </select> */}
+                        <input type="radio" name='conditions' value='Great!' id="conditions1" onChange={(e => setConditions(e.target.value))} />
+                        <label htmlFor="conditions1" className="conditions1"> Great!</label>
+                        
+
                     </label>
                     
                     </div>
