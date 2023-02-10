e50408de41f0973c13dd50b0662a1382792f4c39
Morgan Rector <mmrector@yahoo.com>
2023-02-10 10:40:11 -0800

    backend routes rendering 404

--- app/controllers/api/searches_controller.rb
+++ app/controllers/api/searches_controller.rb
@@ -3,11 +3,11 @@ class Api::SearchesController < ApplicationController
     def search_filter
         query = params[:query] #94107
         if query.to_i != 0
-            @listings = Hike.where("zip = (?)", query)
+            @hikes = Hike.where("name", query)
             render 'api/hikes/index'
        
         else
-            @listings = Park.where("listing_type = (?)", query.downcase)
+            @parks = Park.where("name", query.downcase)
             render 'api/hikes/index'
         end
     end
--- frontend/src/components/Search/index.jsx
+++ frontend/src/components/Search/index.jsx
@@ -7,9 +7,7 @@ import * as searchbarcss from "../Searchbar/searchbar.css"
 import { useHistory } from 'react-router-dom';
 
 const Search = ({ setSearchOpen, open }) => {
-
     const history = useHistory();
-    const [filteredData, setFilteredData] = useState([])
     const [searchQuery, setSearchQuery] = useState("")
     let hikes = useSelector(getHikes)
     let parks = useSelector(getParks)
@@ -20,58 +18,39 @@ const Search = ({ setSearchOpen, open }) => {
 
     let parksAndHikes = ([parks[0]]).concat([hikes[0]]).concat([parks[1]]).concat(hikes.slice(1, hikes.length)).concat(parks.slice(2, parks.length))
 
-    const handleFilter = (e) => {
-        setSearchOpen(true)
-        const searchedWord = e.target.value;
-        setSearchQuery(searchedWord);
-
-        const filterQuery = parksAndHikes.filter((item) => {
-            return item.name.toLowerCase().includes(searchedWord.toLowerCase());
-        })
-
-        if (searchedWord === "") {
-            setFilteredData(parksAndHikes)
-        } else {
-            setFilteredData(filterQuery);
-        }
+    const handleSearch = (e) => {
+     e.preventDefault();
+     history.push({
+        pathname: "/search",
+        state: {searchQuery}
+     })
     }
 
     const clearSearchBarFields = () => {
-        setFilteredData([]);
         setSearchQuery("")
     }
 
-    const handleGreenArrowClick = () => {
-        if (open) {
-            const firstItem = filteredData[0];
-            return history.push(`/hikes/${firstItem.id}`)
-        }
-
-    }
+    
 
     return (
         <div className="search-container">
             <div className="search-input">
-
-                <input type='text' value={searchQuery} placeholder="Search by hike or park name" onChange={handleFilter} onClick={handleFilter} className="search-input-bar" />
+                <input type='text' value={searchQuery} placeholder="Search by hike or park name" onChange={handleSearch} className="search-input-bar" />
                 <i className="fa-solid fa-magnifying-glass search-icon"></i>
-                <button className="go-to-show-page" onClick={handleGreenArrowClick}><i className="fa-solid fa-arrow-right"></i></button>
+                <button className="go-to-show-page"> <i className="fa-solid fa-arrow-right"></i></button>
             </div>
             {open && (
                 <div className="search-results-container">
                     <div className="options">
                         <p className='searchbar-options'>All</p>
                     </div>
-                    {filteredData.length !== 0 ? filteredData.map((item, i) => {
-                        return (
-                            <div className='search-results' key={i}>
-                              
-                            </div>
-                        );
-                    }) : <div className='no-search-results'>
+                        <div className='search-results'>
+                              <p>hello</p>
+                        </div>
+                    {/* <div className='no-search-results'>
                         <p className='searchbar-no-results-message'>Sorry, no results found!</p>
                         <Link to="/hikes" onClick={() => window.scrollTo({ top: 0, left: 0 })} className='no-results-link'><p className='see-bay-area-hikes'>See Bay Area Hikes</p></Link>
-                    </div>}
+                    </div> */}
                 </div>
             )}
         </div>
